a1827c2a7f88b5fafe051fd2023100cf8b1868d1
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2016-12-10 18:17:39 +0100

    Add result types

--- src/main/scala/edu/pw/automata/fsm/DFA.scala
+++ src/main/scala/edu/pw/automata/fsm/DFA.scala
@@ -9,9 +9,9 @@ class DFA(Q:   States,
           F:   States)
   extends FSM(Q, Σ, δ, q0, F) {
 
-  def move(word: Word) = move(word, q0)
+  def move(word: Word): Option[State] = move(word, q0)
 
-  def move(symbol: Symbol, s: Option[State]) = {
+  def move(symbol: Symbol, s: Option[State]): Option[State] = {
     val d = δ.filter(p => p.symbol == symbol && p.a == s.getOrElse(None))
 
     if(d.isEmpty)
@@ -26,6 +26,8 @@ class DFA(Q:   States,
     case _ => None
   }
 
-  def move(word: String, s: Option[State]) = move(for (x <- word.toList) yield Symbol(x toString), s)
+  def move(word: String, s: Option[State]): Option[State] = {
+    move(for (x <- word.toList) yield Symbol(x toString), s)
+  }
 
 }
