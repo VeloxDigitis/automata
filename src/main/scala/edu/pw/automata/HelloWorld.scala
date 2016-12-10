307ab48d5d82d337c7f730abaa797e289b2bd2b9
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2016-12-10 12:14:02 +0100

    ScalaJS HelloWorld
    Initial commit
    Hello, World
    ScalaJS
    ScalaTags
    .gitignore
    SBT configuration

---
+++ src/main/scala/edu/pw/automata/HelloWorld.scala
@@ -0,0 +1,13 @@
+package edu.pw.automata
+import scala.scalajs.js.annotation.JSExport
+
+@JSExport
+object HelloWorld {
+
+  @JSExport
+  def main(): Unit = {
+    
+	println("Hello, world!")
+	
+  }
+}
