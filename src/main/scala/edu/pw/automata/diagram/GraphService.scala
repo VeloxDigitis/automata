c046bd340bd61e03440af543699f440241d0bc63
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2016-12-29 23:59:21 +0100

    Add controller

--- src/main/scala/edu/pw/automata/diagram/GraphService.scala
+++ src/main/scala/edu/pw/automata/diagram/GraphService.scala
@@ -1,9 +1,17 @@
 package edu.pw.automata.diagram
 
+import edu.pw.automata.fsm.State
 import edu.pw.automata.{DFAService, DOMGlobalScope}
 import io.udash.wrappers.jquery._
 
 object GraphService {
+
+  var current: Option[State] = None
+
+  def move() = {
+    val a = if(current.isEmpty) {DFAService.dfa.getStates.head} else {current.get}
+    current = DFAService.dfa.move("0", Some(a))
+  }
 
 
   def repaint() = {
@@ -14,13 +22,16 @@ object GraphService {
 
     val accepting = dfa.getStates.filter(dfa.isAccepting(_))
 
-    val s = new StringBuilder("digraph finite_state_machine {" +
-      "rankdir=LR;" +
-      "size=\"8,5\"" +
+
+    val s = new StringBuilder("digraph finite_state_machine {rankdir=LR;" +
       "node [shape = doublecircle];" + accepting.mkString(" ") + {if(accepting.isEmpty) ("") else (";")} +
       "node [shape = circle];")
 
     dfa.getDelta.foreach(e => s.append(e.a.toString + " -> " + e.b.toString + " [ label = \"" + e.symbol.toString() + "\" ];"))
+
+    if(current.isDefined)
+      s.append(current.get.toString + " [fillcolor = red, style=filled];")
+
     s.append("}")
 
     jQ("#diagram-main").append(DOMGlobalScope.drawGraph(s.toString()))
--- src/main/scala/edu/pw/automata/views/Diagram.scala
+++ src/main/scala/edu/pw/automata/views/Diagram.scala
@@ -1,14 +1,26 @@
 package edu.pw.automata.views
 
+import edu.pw.automata.diagram.GraphService
 import edu.pw.automata.translations.Translations
+import io.udash.bootstrap.button.UdashButton
 import org.scalajs.dom.Element
 
 import scalatags.JsDom.all._
 
 class Diagram extends Section with TranslatedView{
 
+  val u = UdashButton()(">")
+
+  u.listen{
+    case _ => {
+      GraphService.move()
+      GraphService.repaint()
+    }
+  }
+
   lazy val template = div(
     h3(t(Translations.diagram.title)),
+    u.render,
     div(id := "diagram-main")
   )
 
